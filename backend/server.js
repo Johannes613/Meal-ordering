afb7aed6e83a801849cf257ba962f5d3fc12b8a7
Yohannis Admasu <160431822+Johannes613@users.noreply.github.com>
2025-02-26 12:54:17 +0400

    ser server.js

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,8 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000", // Local development frontend
-      "https://full-stack-food-website-chi.vercel.app", // Deployed frontend URL
+      "https://full-stack-food-website-chi.vercel.app",
+      "https://full-stack-food-frontend.onrender.com"// Deployed frontend URL
       // Add any other URLs that need access
     ],
     methods: ["POST", "GET", "OPTIONS", "PUT", "DELETE"],
