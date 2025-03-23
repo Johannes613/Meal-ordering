9378316adcd6ced6862b19504c0fb48bc51cc632
Yohannis Adamu <160431822+Johannes613@users.noreply.github.com>
2025-03-23 13:17:17 +0400

    Update AppDownload.js

--- frontend/food-delivery/src/components/AppDownload/AppDownload.js
+++ frontend/food-delivery/src/components/AppDownload/AppDownload.js
@@ -3,7 +3,7 @@ import { assets } from '../../assets/assets';
 const AppDownload = () => {
     return ( 
         <div className="app-download">
-            <h1>For Better Experiance Download <br />Tomato App</h1>
+            <h1>For Better Experiance Download <br />Flavour App</h1>
             <div className="app-logo">
                 <img src={assets.play_store} alt="" />
                 <img src={assets.app_store} alt="" />
